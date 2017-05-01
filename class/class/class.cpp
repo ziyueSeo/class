5faee0e4d24d5670867b454797751e6de47444b0
lpc <460488013@qq.com>
2017-05-01 23:29:16 +0800

    add a file

--- class/class/class.cpp
+++ class/class/class.cpp
@@ -11,33 +11,45 @@ private:
 	float high = 1.0;
 	float wide = 1.0;
 public:
-	float area(float le, float hi, float wi)
-	{
-		float are;
-		length = le;
-		high = hi;
-		wide = wi;
-		are = 2 * (length*high + length*wide + high*wide);
-		return are;
-	};
-	float volume(float le, float hi, float wi)
-	{
-		float vol;
-		length = le;
-		high = hi;
-		wide = wi;
-		vol = length*high*wide;
-		return vol;
-	};
+	Cube();
+	Cube(float le, float hi, float wi);
+	float area();
+	float volume();
+	~Cube();
 };
+Cube::Cube()
+{
+	length = 0.0;
+	high = 0.0;
+	wide = 0.0;
+}
+Cube::Cube(float le, float hi, float wi)
+{
+	length = le;
+	high = hi;
+	wide = wi;
+}
+float Cube::area()
+{
+	float are;
+	are = 2 * (length*high + length*wide + high*wide);
+	return are;
+}
+float Cube::volume()
+{
+	float vol;
+	vol = length*high*wide;
+	return vol;
+}
+Cube::~Cube(){}
 int _tmain(int argc, _TCHAR* argv[])
 {
-	Cube cub1;
-	Cube cub2;
-	std::cout << "cub1的面积为:" << cub1.area(12.0, 20.0, 25.0) << endl;
-	std::cout << "cub1的体积为:" << cub1.volume(12.0, 20.0, 25.0) << endl;
-	std::cout << "cub2的面积为:" << cub2.area(10, 14, 20) << endl;
-	std::cout << "cub2的体积为:" << cub2.volume(10, 14, 20) << endl;
+	Cube cub1(12.0,20.0,25.0);
+	Cube cub2=Cube(10,14,20);
+	std::cout << "cub1的面积为:" << cub1.area() << endl;
+	std::cout << "cub1的体积为:" << cub1.volume() << endl;
+	std::cout << "cub2的面积为:" << cub2.area() << endl;
+	std::cout << "cub2的体积为:" << cub2.volume() << endl;
 	system("pause");
 	return 0;
 
